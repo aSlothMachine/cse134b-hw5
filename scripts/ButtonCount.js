f1f6af362ba8f434dc74f0dae58cd827c52a32ed
Richie <76547071+aSlothMachine@users.noreply.github.com>
2023-03-07 15:02:23 -0800

    Part 2

--- scripts/ButtonCount.js
+++ scripts/ButtonCount.js
@@ -1,11 +1,31 @@
-class Button extends HTMLElement {
+class ButtonCount extends HTMLElement {
     constructor() {
         super();
 
-        // Create a shadow root
-        var shadow = this.attachShadow({ mode: 'open' });
+        // Create a shadow root.
+        let shadow = this.attachShadow({ mode: 'open' });
+
+        // button appearance.
+        let button = document.createElement("button");
+
+        // counter variable.
+        let counter = document.createElement("counter"); //may not need counter tbh
+
+        // set button text.
+        counter.innerText = 0;
+        button.innerText = `Times Clicked: ${counter.innerText}`;
+
+        // append to shadow.
+        shadow.appendChild(button);
+        // shadow.appendChild(counter); dont need
+        
+        // user clicks the button.
+        button.addEventListener("click", function() {
+            counter.innerText = Number.parseInt(counter.innerText, 10) + 1;
+            button.innerText = `Times Clicked: ${counter.innerText}`;
+        });
     }
 }
 
 // Define the new element.
-customElements.define('button-count', Button);
+customElements.define('button-count', ButtonCount);
